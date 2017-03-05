89977ff16b2de143679f7005d4c25b9390b3f950
jasonwu <jasonwu950709@gmail.com>
2017-03-05 18:49:28 +1100

    Letter Combination of a Phone Number

---
+++ LetterCombinations.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by jasonwu on 2017/2/24.
+ */
+public class LetterCombinations {
+
+    public List<String> letterCombinations(String digits) {
+        String[] letters = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> result = new LinkedList<>();
+
+        if (digits.length() == 0) {
+            return result;
+        }
+        result.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int num = Character.getNumericValue(digits.charAt(i));
+            while (result.element().length() <= i) {
+                String before = result.remove();
+                for (char letter : letters[num].toCharArray()) {
+                    result.add(before + letter);
+                }
+            }
+        }
+        return result;
+    }
+}
+
--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int x = -2147447412;
-        int y = 123321;
-        boolean result1 = PalindromeNumber.isPalindrome(x);
-        boolean result2 = PalindromeNumber.isPalindrome(y);
-
-        System.out.println(result1);
-        System.out.println(result2);
+        LetterCombinations lc = new LetterCombinations();
+        System.out.println(lc.letterCombinations(""));
     }
 }
