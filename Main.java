48c9b60f2acba28136b7cb7bf8b2cc1f863571f3
Jason Wu <jaaasonwu@gmail.com>
2017-08-30 18:33:20 +1000

    Complete LongestSubstring

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        Permutations perm = new Permutations();
-        int[] nums = {1, 2, 3};
-        System.out.println(perm.permute(nums));
+        LongestSubstring longestSubstring = new LongestSubstring();
+
+        System.out.println(longestSubstring.lengthOfLongestSubstring("abcabcbb"));
+
     }
 }
