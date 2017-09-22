8132ed553a0c6f682065ed56d816e114bea5dc86
Jason Wu <jaaasonwu@gmail.com>
2017-09-23 01:21:02 +1000

    Improved version for find median sorted arrays

--- FindMedianSortedArrays.java
+++ FindMedianSortedArrays.java
@@ -1,39 +1,62 @@
-import java.util.ArrayList;
 
-/**
- * Created by Jason Wu on 2017/9/21.
- */
 public class FindMedianSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int i = 0, j = 0;
+        boolean odd;
         int len1 = nums1.length, len2 = nums2.length;
-        ArrayList<Integer> list = new ArrayList<>();
-        while (i <= len1 && j <= len2) {
-            if (i == len1) {
-                for (; j < len2; j++) {
-                    list.add(nums2[j]);
-                }
-                break;
-            }
+        int total = len1 + len2;
+        int current = 0;
+        int prevVal = 0;
+        double result = 0;
 
-            if (j == len2) {
-                for (; i < len1; i++) {
-                    list.add(nums1[i]);
-                }
-                break;
-            }
-            if (nums1[i] < nums2[j]) {
-                list.add(nums1[i]);
-                i++;
+
+        if (total % 2 == 0) {
+            odd = false;
+        } else {
+            odd = true;
+        }
+
+        if (len1 == 0) {
+            if (odd) {
+                return nums2[len2 / 2];
             } else {
-                list.add(nums2[j]);
-                j++;
+                return (double) (nums2[len2 / 2 - 1] + nums2[len2 / 2]) / 2;
             }
         }
 
-        int listLen = list.size();
-        return listLen % 2 == 0 ? (double) (list.get(listLen / 2 - 1) + list.get(listLen / 2)) / 2
-                : list.get(listLen / 2);
+        if (len2 == 0) {
+            if (odd) {
+                return nums1[len1 / 2];
+            } else {
+                return (double) (nums1[len1 / 2 - 1] + nums1[len1 / 2]) / 2;
+            }
+        }
+
+        while (current <= total / 2) {
+            if (i != len1 && (j == len2 || nums1[i] < nums2[j])) {
+                if (current == total / 2) {
+                    result = nums1[i];
+                }
+                if (!odd && current == total / 2 - 1) {
+                    prevVal = nums1[i];
+                }
+                current++;
+                i++;
+            } else if (i == len1 || nums1[i] >= nums2[j]) {
+                if (current == total / 2) {
+                    result = nums2[j];
+                }
+                if (!odd && current == total / 2 - 1) {
+                    prevVal = nums2[j];
+                }
+                current++;
+                j++;
+            }
+        }
+        if (!odd) {
+            result = (result + prevVal) / 2;
+        }
+        return result;
 
     }
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         FindMedianSortedArrays fm = new FindMedianSortedArrays();
-        int nums1[] = {1, 3};
-        int nums2[] = {2, 4};
+        int nums1[] = {1, 2, 3, 4};
+        int nums2[] = {0};
         double result;
         result = fm.findMedianSortedArrays(nums1, nums2);
         System.out.println(result);
