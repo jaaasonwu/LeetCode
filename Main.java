9a26ebf21ac2e6282f7a24c65c54406d23dbdc6e
jasonwu <jasonwu950709@gmail.com>
2017-12-28 10:01:10 +0800

    Swap Nodes in pairs

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
-        RemoveNthFromEnd rm = new RemoveNthFromEnd();
-        rm.removeNthFromEnd(head, 1);
+//        ListNode sec = new ListNode(2);
+//        head.next = sec;
+        SwapPairs swapPairs = new SwapPairs();
+        swapPairs.swapPairs(head);
         System.out.println(head.val);
     }
 }
