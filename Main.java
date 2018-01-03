750b82269f628e5e1b58733624e25ef82b028e9b
jasonwu <jasonwu950709@gmail.com>
2018-01-03 16:19:43 +0800

    First missing positive

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        SearchRange search = new SearchRange();
-        int[] nums = new int[]{1, 2, 2};
-        int[] i = search.searchRange(nums, 2);
-        System.out.println(i[0]);
-        System.out.println(i[1]);
+        FirstMissingPositive fmp = new FirstMissingPositive();
+        int[] nums = new int[]{-1,4,2,1,9,10};
+        int i = fmp.firstMissingPositive(nums);
+        System.out.println(i);
     }
 }
