459e069198eb154fe1491a5652d94f06efc07c7a
Jason Wu <jaaasonwu@gmail.com>
2018-02-28 14:47:05 +1100

    Longest common prefix

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        FirstMissingPositive fmp = new FirstMissingPositive();
-        int[] nums = new int[]{-1,4,2,1,9,10};
-        int i = fmp.firstMissingPositive(nums);
-        System.out.println(i);
+        String[] strings = {};
+        LongestCommonPrefix lcp = new LongestCommonPrefix();
+        System.out.println(lcp.longestCommonPrefix(strings));
     }
 }
