603f9e91983463a4315488deb3956d6fe7b94087
Jason Wu <jaaasonwu@gmail.com>
2018-05-23 14:08:37 +1000

    Two versions of combination sum (allows repeating)

---
+++ CombinationSum2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/**
+ * Created by Jason Wu on 2018/3/2.
+ */
+public class CombinationSum2 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(candidates);
+        backTrack(result, new ArrayList<>(), candidates, target, 0);
+
+        return new ArrayList<>(result);
+    }
+
+    public void backTrack(Set<List<Integer>> result, List<Integer> temp, int[] candidates, int remain, int start) {
+        if (remain == 0) {
+            result.add(new ArrayList<>(temp));
+        } else if (remain > 0) {
+            for (int i = start; i < candidates.length; i++) {
+                temp.add(candidates[i]);
+                backTrack(result, temp, candidates, remain - candidates[i], i + 1);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        CombinationSum cs = new CombinationSum();
-        System.out.println(cs.combinationSum2(new int[]{2, 3, 4, 7}, 500));
+        CombinationSum2 cs = new CombinationSum2();
+        System.out.println(cs.combinationSum2(new int[]{3, 1, 3, 5, 1, 1}, 8));
     }
 }
