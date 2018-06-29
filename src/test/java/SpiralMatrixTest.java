8f04f3fa5bf323e7fb28109b6aee675a69cf9597
Jason Wu <jaaasonwu@gmail.com>
2018-06-29 22:30:39 +1000

    Finished spiral matrix

---
+++ src/test/java/SpiralMatrixTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import problems.SpiralMatrix;
+
+public class SpiralMatrixTest {
+    @Test
+    public void spiralMatrixTest () {
+        SpiralMatrix sm = new SpiralMatrix();
+        System.out.println(sm.spiralOrder(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}}));
+    }
+}
