2251fff5b20605ec6ef92659873681f937f9d3ac
Jason Wu <jaaasonwu@gmail.com>
2018-07-05 21:45:17 +1000

    Understand shortest subarray with sum at least k

---
+++ src/test/java/ShortestSubarrayTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import problems.ShortestSubarrayWithSumAtLeastK;
+
+public class ShortestSubarrayTest {
+    @Test
+    public void testShortestSubarray () {
+        ShortestSubarrayWithSumAtLeastK ss = new ShortestSubarrayWithSumAtLeastK();
+        System.out.println(ss.shortestSubarray(new int[]{2, -1, 2}, 3));
+    }
+}
