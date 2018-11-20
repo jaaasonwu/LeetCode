68d7f8b2d10019a7bc7e3784cecdb9da7abecce5
jaaasonwu <jaaasonwu@gmail.com>
2018-11-20 11:09:10 +0800

    Add a iterative way to find the next node

--- src/main/java/problems/TreeNextInorderNode.java
+++ src/main/java/problems/TreeNextInorderNode.java
@@ -34,4 +34,35 @@ public class TreeNextInorderNode {
 
         return findParent(curr.next);
     }
+
+    public TreeLinkNode GetNext2(TreeLinkNode pNode)
+    {
+        if (pNode == null) {
+            return null;
+        }
+
+        if (pNode.right != null) {
+            return findLeftMost2(pNode.right);
+        } else {
+            return findParent2(pNode);
+        }
+    }
+
+    private TreeLinkNode findLeftMost2 (TreeLinkNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+
+        return root;
+    }
+
+    private TreeLinkNode findParent2 (TreeLinkNode curr) {
+        while (curr.next != null) {
+            if (curr.next.left == curr) {
+                return curr.next;
+            }
+            curr = curr.next;
+        }
+        return null;
+    }
 }
