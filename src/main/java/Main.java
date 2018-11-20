a417bb0050b8030bab5aa82953221c486c1e534e
jaaasonwu <jaaasonwu@gmail.com>
2018-11-20 12:54:34 +0800

    Completed fibonacci

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,7 @@ import problems.*;
 public class Main {
 
     public static void main(String[] args) {
-        TreeNextInorderNode tn = new TreeNextInorderNode();
-        TreeLinkNode root = new TreeLinkNode(1);
-        TreeLinkNode node2 = new TreeLinkNode(2);
-        root.right = node2;
-        node2.next = root;
-        TreeLinkNode node3 = new TreeLinkNode(3);
-        node2.right = node3;
-        node3.next = node2;
-        System.out.println(tn.GetNext(node3));
+        Fibonacci fi = new Fibonacci();
+        System.out.println(fi.Fibonacci(5));
     }
 }
---
+++ src/main/java/problems/Fibonacci.java
@@ -0,0 +1,24 @@
+package problems;
+
+public class Fibonacci {
+    public int Fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int res = 0;
+        int prev = 1;
+        int prevprev = 0;
+        for (int i = 2; i <= n; i++) {
+            res = prev + prevprev;
+            prevprev = prev;
+            prev = res;
+        }
+
+        return res;
+    }
+}
