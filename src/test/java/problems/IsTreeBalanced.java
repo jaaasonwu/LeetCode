1d0ea191fa9b52eaf867090222d020bcc737e5f0
Jason Wu <jaaasonwu@gmail.com>
2018-12-06 18:08:26 +0800

    Simplify code for finding if a tree is balanced

--- src/test/java/problems/IsTreeBalanced.java
+++ src/test/java/problems/IsTreeBalanced.java
@@ -8,20 +8,16 @@ public class IsTreeBalanced {
     }
 
     private int isBalancedRecursive(TreeNode root) {
-        if (root.left == null && root.right == null) {
+        if (root == null) {
             return 0;
         }
 
-        int left = -1, right = -1;
-        if (root.left != null) {
-            left = isBalancedRecursive(root.left);
-        }
+        int left = 0, right = 0;
+        if (root.left != null) left = isBalancedRecursive(root.left);
+        if (left == -1) return -1;
 
-        if (root.right != null) {
-            right = isBalancedRecursive(root.right);
-        }
-
-        if (left == -1 && right == -1) return -1;
+        if (root.right != null) right = isBalancedRecursive(root.right);
+        if (right == -1) return -1;
         if (Math.max(left, right) - Math.min(left, right) > 1) {
             return -1;
         } else {
